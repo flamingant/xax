32b7b809feff466644e3701f681135a2757d5ace
unknown <nick@boo.(none)>
2013-12-06 16:33:27 +0000

    change to just build the library

--- lgen.pm
+++ lgen.pm
@@ -1,18 +1,6 @@
 ################################################################
 use npulgen ;
 
-push @linkgroups,"AOBJSDEP" ;
-push @linkgroups,"AOBJSNODEP" ;
-
-macro "AEXE",'tc$(DOTEXE)' ;
-
-push @targets,'$(AEXE)' ;
+push @targets,'libnpu.a' ;
  
-sub object_a { object @_,linkgroup => "AOBJSDEP"}
-
-object '$(SQLITE)/$(ARCH)/sqlite3.o',linkgroup => "AOBJSNODEP" ;
-
-object_a "main.c" ;
-object_a "common.c" ;
-
 1;
