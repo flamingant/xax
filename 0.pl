a99a710c30bde3f0810768c7b43079b7bac0b7f5
nicko <nick@boo>
2014-01-09 20:13:45 +0000

    catch up

--- 0.pl
+++ 0.pl
@@ -30,13 +30,21 @@ sub _1222_1027 {
     shell "git push -n -v smarf" ;
 }
 
+sub _1222_1101 { shell "git push -n -v smarf HEAD" }
+
+sub _1222_1206 { shell "git push -n -v smarf lib" }
+
+sub _1222_1359 { shell "git remote rm smarf" }   # moving to better location
+
 __END__
 
 =pod
 remote smarf is on branch ant. We need to fast forward ant to current work
 because we can only (?) push back the branches that exist in the remote
 repository. If we have created new branches that don't exist in the remote
-what happens if we try to push back
+what happens if we try to push back branches that don't exist?
+
+It doesn't matter what the current branch is
 
 this would be more straightforward if we only had one remote 
 =cut
